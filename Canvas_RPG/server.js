9cf6fc174d44652919f944ceb7589fb86e985ed3
Marty808s <134133128+Marty808s@users.noreply.github.com>
2025-12-09 12:48:00 +0100

    init

---
+++ Canvas_RPG/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config({ path: "../.env" });
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// testovací route
+app.get("/api/health", (req, res) => {
+  res.json({ ok: true, message: "Backend běží 🚀" });
+});
+
+// tady pak přidáš /api/register, /api/login, /api/save ...
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server běží na http://localhost:" + PORT);
+});
